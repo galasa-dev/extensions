37678b28384741ae925479be4ff7a643c372124c
James Davies <james.daviesuk.ibm.com@jamess-mbp.hursley.uk.ibm.com>
2019-01-24 10:20:56 +0000

    Sonar Test

---
+++ ejat-core-parent/ejat-core-dummy/src/main/java/com/ibm/ejatcore/EjatCore.java
@@ -0,0 +1,10 @@
+package ejatcore;
+
+public class EjatCore{
+    
+    public static String dummyMethod() {
+        String line = "Hello World!";
+        System.out.println(line);
+        return line;
+    }
+}
