b6b9615dc34bd2893c80517ca95c9dba55860c3d
James Davies <james.daviesuk.ibm.com@jamess-mbp.hursley.uk.ibm.com>
2019-01-24 11:36:40 +0000

    Further sonar test

--- ejat-core-parent/ejat-core-dummy/src/test/java/com/ibm/ejatcore/EjatCoreTest.java
+++ ejat-core-parent/ejat-core-dummy/src/test/java/com/ibm/ejatcore/EjatCoreTest.java
@@ -3,10 +3,17 @@ package ejatcore;
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.beans.Transient;
+
 public class EjatCoreTest{
     
     @Test
     public void test_method() {
         assertNotNull(EjatCore.dummyMethod());
+    }
+
+    @Test
+    public void test_method_2() {
+        assertTrue(true);
     }
 }
