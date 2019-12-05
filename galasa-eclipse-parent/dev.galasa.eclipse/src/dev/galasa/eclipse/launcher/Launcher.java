c9cc4f7304ead86f5696dae783d4fd346e79661a
Michael Baylis <Michael.Baylis@uk.ibm.com>
2019-12-05 11:29:21 +0000

    Update uber.obr version

--- galasa-eclipse-parent/dev.galasa.eclipse/src/dev/galasa/eclipse/launcher/Launcher.java
+++ galasa-eclipse-parent/dev.galasa.eclipse/src/dev/galasa/eclipse/launcher/Launcher.java
@@ -240,7 +240,7 @@ public class Launcher extends JavaLaunchDelegate {
         programArguments.add("--obr");
         programArguments.add(workspaceOBR.toURI().toString());
         programArguments.add("--obr");
-        programArguments.add("mvn:dev.galasa/dev.galasa.uber.obr/0.3.0/obr");
+        programArguments.add("mvn:dev.galasa/dev.galasa.uber.obr/0.3.1/obr");
         if (trace) {
             programArguments.add("--trace");
         }
