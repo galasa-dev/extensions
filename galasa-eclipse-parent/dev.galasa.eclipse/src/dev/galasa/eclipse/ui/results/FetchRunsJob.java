3a619aa3f2e93822e7af61d6bba0d265d279b134
CJMeek <up887397@myport.ac.uk>
2020-09-24 13:58:16 +0100

    extensions updated for new getRuns() method
    Signed-off-by: CJMeek <up887397@myport.ac.uk>

--- galasa-eclipse-parent/dev.galasa.eclipse/src/dev/galasa/eclipse/ui/results/FetchRunsJob.java
+++ galasa-eclipse-parent/dev.galasa.eclipse/src/dev/galasa/eclipse/ui/results/FetchRunsJob.java
@@ -18,6 +18,11 @@ import dev.galasa.eclipse.ui.IRunResultsListener;
 import dev.galasa.framework.spi.IFramework;
 import dev.galasa.framework.spi.IResultArchiveStoreDirectoryService;
 import dev.galasa.framework.spi.IRunResult;
+import dev.galasa.framework.spi.ras.IRasSearchCriteria;
+import dev.galasa.framework.spi.ras.RasSearchCriteriaQueuedFrom;
+import dev.galasa.framework.spi.ras.RasSearchCriteriaQueuedTo;
+import dev.galasa.framework.spi.ras.RasSearchCriteriaRequestor;
+import dev.galasa.framework.spi.ras.RasSearchCriteriaTestName;
 
 public class FetchRunsJob extends Job {
 
@@ -25,11 +30,10 @@ public class FetchRunsJob extends Job {
     private final IResultArchiveStoreDirectoryService dirService;
     private final String                              requestor;
     private final String                              testClass;
-    private final String							  testName;
     private final Instant                             from;
     private final Instant                             to;
 
-    public FetchRunsJob(String requestor, String testClass, Instant from, Instant to, String testName,
+    public FetchRunsJob(String requestor, String testClass, Instant from, Instant to,
             IResultArchiveStoreDirectoryService dirService, IRunResultsListener listener) {
         super("Fetch Runs");
 
@@ -37,7 +41,6 @@ public class FetchRunsJob extends Job {
         this.dirService = dirService;
         this.requestor = requestor;
         this.testClass = testClass;
-        this.testName = testName;
         this.from = from;
         this.to = to;
 
@@ -46,14 +49,20 @@ public class FetchRunsJob extends Job {
 
     @Override
     protected IStatus run(IProgressMonitor monitor) {
-
+    	
+    	RasSearchCriteriaRequestor requestorCrit = new RasSearchCriteriaRequestor(requestor);
+    	RasSearchCriteriaQueuedFrom fromCrit = new RasSearchCriteriaQueuedFrom(from);
+    	RasSearchCriteriaQueuedTo toCrit = new RasSearchCriteriaQueuedTo(to);
+    	
+    	IRasSearchCriteria[] criteria = {requestorCrit, fromCrit, toCrit};
+    	
         try {
             IFramework framework = Activator.getInstance().getFramework();
             if (!framework.isInitialised()) {
                 return new Status(Status.OK, Activator.PLUGIN_ID, "Runs not fetched - Framework not intialised");
             }
-
-            List<IRunResult> runs = dirService.getRuns(requestor, from, to, testName);
+           
+            List<IRunResult> runs = dirService.getRuns(criteria);
 
             listener.runsUpdate(runs);
         } catch (Exception e) {
