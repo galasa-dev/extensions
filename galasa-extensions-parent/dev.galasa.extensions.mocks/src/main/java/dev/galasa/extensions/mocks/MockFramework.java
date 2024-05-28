5aa252602406a20903cb1e30948f8717ae9fd406
Jade Carino <carino_jade@yahoo.co.uk>
2024-05-28 14:50:07 +0100

    Implement methods in MockFramework and MFInitialisation
    Signed-off-by: Jade Carino <carino_jade@yahoo.co.uk>

--- galasa-extensions-parent/dev.galasa.extensions.mocks/src/main/java/dev/galasa/extensions/mocks/MockFramework.java
+++ galasa-extensions-parent/dev.galasa.extensions.mocks/src/main/java/dev/galasa/extensions/mocks/MockFramework.java
@@ -19,6 +19,7 @@ import dev.galasa.framework.spi.ICertificateStoreService;
 import dev.galasa.framework.spi.IConfidentialTextService;
 import dev.galasa.framework.spi.IConfigurationPropertyStoreService;
 import dev.galasa.framework.spi.IDynamicStatusStoreService;
+import dev.galasa.framework.spi.IEventsService;
 import dev.galasa.framework.spi.IFramework;
 import dev.galasa.framework.spi.IFrameworkRuns;
 import dev.galasa.framework.spi.IResourcePoolingService;
@@ -122,6 +123,11 @@ public class MockFramework implements IFramework {
     @Override
     public @NotNull IAuthStore getAuthStore() {
         throw new UnsupportedOperationException("Unimplemented method 'getAuthStore'");
+    }
+
+    @Override
+    public @NotNull IEventsService getEventsService() {
+        throw new UnsupportedOperationException("Unimplemented method 'getEventsService'");
     }
 
 }
