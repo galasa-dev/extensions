eb01bc3ca0166ae2b1ed4951ae5966ccab6c926b
Jade Carino <carino_jade@yahoo.co.uk>
2024-06-16 15:31:50 +0100

    Add codecoverage for the Kafka plugin
    Signed-off-by: Jade Carino <carino_jade@yahoo.co.uk>

--- build-locally.sh
+++ build-locally.sh
@@ -208,6 +208,19 @@ function displayCouchDbCodeCoverage {
     info "See html report here: file://${BASEDIR}/galasa-extensions-parent/dev.galasa.ras.couchdb/build/jacocoHtml/index.html"
 }
 
+function displayKafkaCodeCoverage {
+    h2 "Calculating Kafka code coverage..."
+    percent_code_complete=$(cat ${BASEDIR}/galasa-extensions-parent/dev.galasa.events.kafka/build/jacocoHtml/dev.galasa.events.kafka.internal/index.html \
+    | sed "s/.*<td>Total<\/td>//1" \
+    | cut -f1 -d'%' \
+    | sed "s/.*>//g")
+    info 
+    info
+    info "Statement code coverage is ${percent_code_complete}%"
+    info
+    info "See html report here: file://${BASEDIR}/galasa-extensions-parent/dev.galasa.events.kafka/build/jacocoHtml/index.html"
+}
+
 function check_secrets {
     h2 "updating secrets baseline"
     cd ${BASEDIR}
@@ -235,6 +248,7 @@ function check_secrets {
 clean_maven_repo
 build_with_gradle
 displayCouchDbCodeCoverage
+displayKafkaCodeCoverage
 check_secrets
 
 success "Project ${project} built - OK - log is at ${log_file}"
